f86f3fcb55cc79120e74e9d8454ae9df37578f76
fitri-ws <wsfitri39@gmail.com>
2025-06-13 16:55:16 +0900

    Delete GameLauncher.java

--- GameLauncher.java
+++
@@ -1,8 +0,0 @@
-
-public class GameLauncher {
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(() -> {
-            new MainMenu().setVisible(true);
-        });
-    }
-}
